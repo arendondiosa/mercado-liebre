ed5c7616c459932acf21f7ab59535ab07ca7897f
Alejandro E. Rendon <alejandro@rendon.co>
2022-07-04 19:07:43 -0500

    Modificar variable de entorno para el puerto

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
-app.listen(3000, () => console.log("Server running in port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server running in port 3000"));
