04f12089977e4f3f5f261051f403cb878372c32a
Alejandro E. Rendon <alejandro@rendon.co>
2022-07-04 19:18:14 -0500

    Modificar variable del puerto y crear Procfile

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running in port 3000"));
+app.listen(port, () => console.log(`Server running in port ${port}`));
